7c46d85d8410e1e40739011649d347db9a2c0264
tanushree-coder-girl <tanushreeahir1999@gmail.com>
2021-08-27 17:38:40 +0530

    js

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log('hello world'); //for printing in console
+
+/* ============= VARIABLES =============== */
+var name = "nk";
+console.log(name);
+var my_name = "nk2";
+console.log(my_name);
+var $surname = "sharma"
+console.log($surname);
+
+/* ============= DATA TYPES ============== */
+var name ;                                  //i am undefined bcoz i have no value
+console.log(typeof(name))
+
+var full_name = "This is a mango";          //string data type
+console.log(typeof(full_name))
+
+var phone_number = 9876543210;              //nmuber data type
+console.log(typeof(phone_number))
+
+var is_login = true;                    //BOOLEAN Data type return only true and false
+console.log(typeof(is_login))
+
+var varialbeNull = null;               // Null DAta Type
+console.log(typeof(varialbeNull))
+
+console.log(isNaN(phone_number)); //it check phone is not a number
+console.log(isNaN(full_name)); //it check full_name is a number or not
+
+console.log(5  - "tanu") //it return nan number
